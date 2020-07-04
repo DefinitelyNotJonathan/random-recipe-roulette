f2ac9524c479de47061d6c5f4c8f2de4a2b410a3
Jonathan <jonretchless@gmail.com>
2020-07-03 22:22:16 -0700

    favicon added

---
+++ src/LogoutButton.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LogoutButton from './LogoutButton.js';
+import { BrowserRouter } from 'react-router-dom';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<BrowserRouter><LogoutButton /></BrowserRouter>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
